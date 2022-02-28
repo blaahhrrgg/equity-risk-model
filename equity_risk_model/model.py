fe8f619d85b2b01a5ef7ddc12cb900bbec423d07
blaahhrrgg <josephmaestri@gmail.com>
2022-02-28 17:51:05 +0000

    add ability to define factor group

--- equity_risk_model/model.py
+++ equity_risk_model/model.py
@@ -1,6 +1,8 @@
 import dataclasses
+from typing import Dict, List, Union
 
 import numpy
+import pandas
 
 
 @dataclasses.dataclass
@@ -18,6 +20,9 @@ class FactorRiskModel:
     loadings: numpy.array
     covariance_factor: numpy.array
     covariance_specific: numpy.array
+    factor_group_mapping: Dict[str, str] = dataclasses.field(
+        default_factory=lambda: {}
+    )
 
     @property
     def n_assets(self) -> int:
@@ -40,6 +45,49 @@ class FactorRiskModel:
             The number of factors in the factor model
         """
         return len(self.factors)
+
+    @property
+    def factor_groups(self) -> List[str]:
+        """Factor groups in the factor model
+
+        Returns
+        -------
+        List[str]
+            A list of factor groups in the factor model (if no factor groups
+            have been specified then returns an empty list)
+        """
+        return list(self.factor_group_mapping.keys())
+
+    @property
+    def factor_index(self) -> Union[pandas.MultiIndex, pandas.Index]:
+        """Returns index for factors
+
+        Returns
+        -------
+        Union[pandas.MultiIndex, pandas.Index]
+            An index that can be used for factor ouputs, if a factor group
+            mapping is provided, then the index will be a multilevel index.
+        """
+
+        if self.factor_group_mapping:
+
+            idx = []
+
+            for factor in self.factors:
+
+                group = None
+
+                for factor_group, factors in self.factor_group_mapping.items():
+                    if factor in factors:
+                        group = factor_group
+
+                idx.append((group, factor))
+
+            return pandas.MultiIndex.from_tuples(
+                idx, names=["FactorGroup", "Factor"]
+            )
+
+        return pandas.Index(self.factors, name="Factor")
 
     @property
     def covariance_total(self) -> numpy.array:
