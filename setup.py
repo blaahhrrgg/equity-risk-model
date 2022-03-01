e98c94bb5652a6390830262b3bda5d4a58af9d04
blaahhrrgg <josephmaestri@gmail.com>
2022-03-01 09:29:07 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ tests_require = ["pytest>=6.1.1", "coverage>=5.3", "pytest-cov>=2.10.1"]
 
 setuptools.setup(
     name="equity-risk-model",
-    version="0.0.2",
+    version="0.0.3",
     description="Portfolio analysis using an equity multi-factor risk model.",
     packages=setuptools.find_packages(),
     install_requires=["cvxpy", "numpy", "scipy"],
